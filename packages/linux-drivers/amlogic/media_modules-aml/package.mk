f2dbaac35c2a9513c3a2e76293e4faa153c360a1
adamg <adamg88@users.noreply.github.com>
2020-02-13 14:30:51 +0000

    media_modules-aml: bump package to f84cae8

--- packages/linux-drivers/amlogic/media_modules-aml/package.mk
+++ packages/linux-drivers/amlogic/media_modules-aml/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="media_modules-aml"
-PKG_VERSION="956410db938b2f04514010d775e79090e9272c95"
-PKG_SHA256="836b95609363f2ce92011f49aad94b3b5b3aa1be3f6caf7b3927742f9999aa59"
+PKG_VERSION="f84cae89d0b79add98cc2d36b57ec6ff7d6fd7b2"
+PKG_SHA256="1aa5a6d4ca4364bffe886e4d09b0e36e7a07b718e2a822becd52f5a7c3bb1c02"
 PKG_LICENSE="GPL"
 PKG_SITE="https://coreelec.org"
 PKG_URL="https://github.com/CoreELEC/media_modules-aml/archive/$PKG_VERSION.tar.gz"
